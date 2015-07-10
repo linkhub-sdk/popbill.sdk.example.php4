a543b056c4a9aa23bd5439bc43ba7fdcffe01962
JohnJeong123 <frenchofkiss@gmail.com>
2015-07-10 15:33:49 +0900

    added closedown example

--- CashbillExample/common.php
+++ CashbillExample/common.php
@@ -2,7 +2,7 @@
 
 require_once '../Popbill/PopbillCashbill.php';
 
-//연동 아이디
+//링크 아이디
 $LinkID = 'TESTER';
 //발급받은 비밀키. 유출에 주의하시기 바랍니다.
 $SecretKey = 'jkjiPCCWfjb9yItA2CMxemSQDVEyBxZg0iymxZvaWKc=';
